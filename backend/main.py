f89851ed7586697c2fa02e85ac88683c329adcb7
Reasondro <alessandrohasian16@gmail.com>
2025-08-15 04:12:48 +0700

    refactor: enhance type hints and update .gitignore

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,19 @@
 import os
 import textwrap
 import uvicorn
-from typing import TypedDict
+from typing import TypedDict, Union, Any
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.documents import Document
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_core.prompt_values import PromptValue
+from langchain_core.vectorstores import InMemoryVectorStore, VectorStoreRetriever
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langgraph.graph import START, StateGraph
+from langgraph.graph import START
+from langgraph.graph.state import CompiledStateGraph, StateGraph
 from pydantic import BaseModel
 
 _ = load_dotenv()
@@ -20,8 +22,10 @@ if not os.environ.get("GOOGLE_API_KEY"):
     raise ValueError("GOOGLE_API_KEY environment variable not set!")
 
 try:
-    llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
-    embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
+    llm: ChatGoogleGenerativeAI = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+    embeddings: GoogleGenerativeAIEmbeddings = GoogleGenerativeAIEmbeddings(
+        model="gemini-embedding-001"
+    )
 except Exception as e:
     print(e)
     exit(1)  #  this counts as abnormal exit, right?
@@ -33,19 +37,19 @@ web_links: list[str] = [
     "https://eform.bni.co.id/BNI_eForm/disclaimerPenawaran",
 ]
 
-loader = WebBaseLoader(web_paths=web_links)
+loader: WebBaseLoader = WebBaseLoader(web_paths=web_links)
 docs: list[Document] = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(
+text_splitter: RecursiveCharacterTextSplitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,
     chunk_overlap=200,
     add_start_index=True,
 )
 all_splits: list[Document] = text_splitter.split_documents(docs)
 
-vector_store = InMemoryVectorStore(embedding=embeddings)
+vector_store: InMemoryVectorStore = InMemoryVectorStore(embedding=embeddings)
 _ = vector_store.add_documents(documents=all_splits)
-retriever = vector_store.as_retriever(
+retriever: VectorStoreRetriever = vector_store.as_retriever(
     search_type="mmr",
     search_kwargs={
         "k": 6,
@@ -60,7 +64,7 @@ template: str = textwrap.dedent(
     Jika nama Bank tidak ada di `context`, katakan Bank tidak ada di basis data dan berhenti."""
 )
 
-prompt_template = ChatPromptTemplate.from_messages(
+prompt_template: ChatPromptTemplate = ChatPromptTemplate.from_messages(
     [
         (
             "system",
@@ -83,21 +87,21 @@ class State(TypedDict):
     answer: str
 
 
-def retrieve(state: State):
+def retrieve(state: State) -> dict[str, list[Document]]:
     """
     Retrieve contexts from the vector store
     """
     print(f"Retrieving documents for question: {state['question']}")
-    retrieved_docs = retriever.invoke(state["question"])
+    retrieved_docs: list[Document] = retriever.invoke(state["question"])
     return {"context": retrieved_docs}
 
 
-def generate(state: State):
+def generate(state: State) -> dict[str, Union[str, list[Union[str, dict]]]]:
     """
     Generate answer based on the retreived context
     """
-    docs_content = "".join(doc.page_content for doc in state["context"])
-    prompt = prompt_template.invoke(
+    docs_content: str = "".join(doc.page_content for doc in state["context"])
+    prompt: PromptValue = prompt_template.invoke(
         {
             "language": state["language"],
             "question": state["question"],
@@ -105,19 +109,20 @@ def generate(state: State):
             "history": state["history"],
         }
     )
-    response = llm.invoke(prompt)
-    return {"answer": response.content}
+    response: BaseMessage = llm.invoke(prompt)
+    content: Union[str, list[Union[str, dict]]] = response.content
+    return {"answer": content}
 
 
-graph_builder = StateGraph(State)
+graph_builder: StateGraph[State] = StateGraph(State)
 _ = graph_builder.add_node("retrieve", retrieve)
 _ = graph_builder.add_node("generate", generate)
 _ = graph_builder.add_edge(START, "retrieve")
 _ = graph_builder.add_edge("retrieve", "generate")
 # TODO: add memory
-graph = graph_builder.compile()
+graph: CompiledStateGraph = graph_builder.compile()
 
-app = FastAPI(
+app: FastAPI = FastAPI(
     title="AIMSURE RAG API",
 )
 
@@ -133,7 +138,7 @@ class QueryResponse(BaseModel):
 
 
 @app.post("/invoke", response_model=QueryResponse)
-async def invoke_chain(request: QueryRequest):
+async def invoke_chain(request: QueryRequest) -> dict[str, Any]:
     hist: list[BaseMessage] = []
     if request.history:
         for sender, text in request.history:
@@ -150,7 +155,7 @@ async def invoke_chain(request: QueryRequest):
         "answer": "",
     }
 
-    result = graph.invoke(initial_state)
+    result: dict[str, Any] | Any = graph.invoke(initial_state)
     return {"answer": result.get("answer", "Sorry, I couldn't find an answer.")}
 
 
